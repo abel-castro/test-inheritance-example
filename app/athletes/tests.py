f5036db38905320b044659172b879ad3bd7c5bde
Abel <abel@castro.at>
2021-03-26 12:07:06 +0100

    refactor tests with test inheritance

--- app/athletes/tests.py
+++ app/athletes/tests.py
@@ -1,3 +1,6 @@
+from pytest import mark
+
+
 from django.test import TestCase
 from django.urls import reverse_lazy
 
@@ -6,7 +9,7 @@ from .serializers import BasketballPlayerSerializer, SoccerPlayerSerializer
 
 
 def create_test_basketball_player():
-    return BasketballPlayer.objects.create(
+    basketball_player, created = BasketballPlayer.objects.get_or_create(
         name="Lebron James",
         slug="lebron-james",
         age=36,
@@ -14,10 +17,11 @@ def create_test_basketball_player():
         assists=9700,
         rebounds=9800,
     )
+    return basketball_player
 
 
 def create_test_soccer_player():
-    return SoccerPlayer.objects.create(
+    soccer_player, created = SoccerPlayer.objects.get_or_create(
         name="Lionel Messi",
         slug="lionel-messi",
         age=33,
@@ -25,29 +29,38 @@ def create_test_soccer_player():
         assists=291,
         yellow_cards=81,
     )
+    return soccer_player
 
 
-class TestBasketballPlayerAPI(TestCase):
+class AthleteTest(TestCase):
+    __test__ = False
+    view_name = ""
+    serializer_class = None
+
     def setUp(self) -> None:
-        self.basketball_player = create_test_basketball_player()
+        self.test_athlete = None
 
+    @mark.django_db
     def test__response_ok(self):
-        url = reverse_lazy(
-            "basketball_player", kwargs={"slug": self.basketball_player.slug}
-        )
+        url = reverse_lazy(self.view_name, kwargs={"slug": self.test_athlete.slug})
         response = self.client.get(url)
-
         assert response.status_code == 200
-        assert response.data == BasketballPlayerSerializer(self.basketball_player).data
+        assert response.data == self.serializer_class(self.test_athlete).data
 
 
-class TestSoccerPlayerAPI(TestCase):
+class TestBasketballPlayerAPI(AthleteTest):
+    __test__ = True
+    view_name = "basketball_player"
+    serializer_class = BasketballPlayerSerializer
+
     def setUp(self) -> None:
-        self.soccer_player = create_test_soccer_player()
+        self.test_athlete = create_test_basketball_player()
 
-    def test__response_ok(self):
-        url = reverse_lazy("soccer_player", kwargs={"slug": self.soccer_player.slug})
-        response = self.client.get(url)
 
-        assert response.status_code == 200
-        assert response.data == SoccerPlayerSerializer(self.soccer_player).data
+class TestSoccerPlayerAPI(AthleteTest):
+    __test__ = True
+    view_name = "soccer_player"
+    serializer_class = SoccerPlayerSerializer
+
+    def setUp(self) -> None:
+        self.test_athlete = create_test_soccer_player()
